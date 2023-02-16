345f5ff63dd0938f69755365326a351d46e9e5c7
Oliver <113297002+oliver748@users.noreply.github.com>
2023-02-16 13:12:44 +0100

    +movie option, +mp4 to mkv, +optimized

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import re
 import os
 
 
-# Xpath for the title of the tv show on dr.dk/drtv website
-TVSHOW_TITLE_XPATH = '//*[@id="row0"]/section/div/div/div/h1'
+# XPath for the title of the tv show on dr.dk/drtv website
+TITLE_XPATH = '//*[@id="row0"]/section/div/div/div/h1'
 
-# Xpath for the available seasons of the tv show on dr.dk/drtv website
+# XPath for the available seasons of the tv show on dr.dk/drtv website
 TVSHOW_SEASONS_XPATH = '//*[@id="season-list-container"]//a'
 
 
@@ -21,18 +21,37 @@ class DRTVScraper:
 
         os.system('cls' if os.name == 'nt' else 'clear')
         
-        tvshow_link = str(input("Enter the link to the show: "))
+        print('Pick an option\n1. TV show\n2. Movie\n')
+        option = int(input('> '))
 
-        tree = self.get_tree(tvshow_link)
-        tvshow_title = self.fetch_tvshow_title(tree)
-        self.season_dic = self.fetch_tvshow_available_seasons(tree, tvshow_link)
-        season_links_to_scrape = self.get_info(tvshow_title, self.season_dic)
+        if option == 1:
+            print('')
+            tvshow_link = str(input("Enter the link to the TV show: "))
+            self.convert_media = str(input("Convert media from .mp4 to .mkv? It's lossless - (y/n): "))
+    
+            tree = self.get_tree(tvshow_link)
+            tvshow_title = self.fetch_title(tree)
+            self.season_dic = self.fetch_tvshow_available_seasons(tree, tvshow_link)
+            season_links_to_scrape = self.get_info(tvshow_title, self.season_dic)
 
-        for season_url in season_links_to_scrape:
-            episode_links = asyncio.run(self.fetch_episode_links(season_url))
-            self.scrape_episodes(episode_links, season_url, tvshow_title)
+            for season_url in season_links_to_scrape:
+                episode_links = asyncio.run(self.fetch_episode_links(season_url))
+                self.scrape_episodes(episode_links, season_url, tvshow_title)
+                
+        elif option == 2:
+            print('')
+            movie_link = str(input('Enter the link to the movie: '))
+            self.convert_media = str(input("Convert media from .mp4 to .mkv? It's lossless - (y/n): "))
+            tree = self.get_tree(movie_link)
+            movie_title = self.fetch_title(tree)
+            self.scrape_movie(movie_title, movie_link)
 
-        print('\nScraping done!')
+        else:
+            print('Seems like you entered an invalid option.')
+            exit()
+
+        print('\nProcess is finished!')
+        input('')
 
 
     def get_tree(self, url):
@@ -44,14 +63,18 @@ class DRTVScraper:
         return tree
 
 
-    def fetch_tvshow_title(self, tree):
+    def fetch_title(self, tree):
         # Extract the title of the tv show
-        tvshow_title = tree.xpath(TVSHOW_TITLE_XPATH)[0].text_content()
+        title = tree.xpath(TITLE_XPATH)[0].text_content()
 
-        # If the folder doesn't exist, create it
-        os.makedirs(tvshow_title, exist_ok=True)
 
-        return tvshow_title
+        # Replace all illegal characters
+        title = re.sub(r'[\\/:*?"<>|]' , ' -', title)
+
+        # Create a folder with the title of the tv show
+        os.makedirs(title, exist_ok=True)
+
+        return title
 
 
     def fetch_tvshow_available_seasons(self, tree, tvshow_link):
@@ -60,7 +83,7 @@ class DRTVScraper:
             tvshow_seasons = tree.xpath(TVSHOW_SEASONS_XPATH)
         except: 
             # returns [1] if there is only one season since the xpath crashes if there is only one season
-            return [1]
+            return {'1': tvshow_link}
 
         # Create a dictionary to store the extracted items
         dic = {}
@@ -85,17 +108,23 @@ class DRTVScraper:
 
         # Splits up the list into a string separated by commas
         seasons = ", ".join(str(season) for season in all_available_seasons)
-
+        
         print(f'\nTV Show name: {tvshow_title}')
         print(f'Seasons available: {seasons}')
         print('\nWhich seasons would you like to download?\n')
         seasons_to_scrape = input('> ')
         
-        # Splits them up and adds them to a list so if user types 1, 2, 3 or 1 and 2 and 3, it will still be ['1', '2', '3']
+        # Extracts all the valid numbers from the input
         seasons_to_scrape = re.findall(r'\d+', seasons_to_scrape)
 
         # Convert strings to integers
         seasons_to_scrape = [int(num) for num in seasons_to_scrape]
+
+        # Check if seasons_to_scrape input is valid and if not, exit
+        for season in seasons_to_scrape:
+            if str(season) not in season_list:
+                print(f'\nSeason {season} is not available. Please try again.')
+                exit()
 
         # Sort the list
         seasons_to_scrape.sort()
@@ -110,9 +139,7 @@ class DRTVScraper:
 
 
     async def fetch_episode_links(self, season_url):
-        # Create a list to store the episode links
-        episode_links = []
-        
+
         # Launch browser and create new page
         browser = await launch()
         page = await browser.newPage()
@@ -139,6 +166,9 @@ class DRTVScraper:
 
         # Loop through each episode element and extract link
         episode_count = 0
+
+        # Create a list to store the episode links
+        episode_links = []
         while True:
             try:
                 # Get the episode link and append it to the list
@@ -159,10 +189,9 @@ class DRTVScraper:
                 except:
                     pass
 
-            except:
+            except: # If there are no more episodes, it will break the loop
                 break
         
-        # close browser and return episode links
         await browser.close()
         return episode_links
 
@@ -176,41 +205,48 @@ class DRTVScraper:
             # Format season and episode numbers as zero-padded strings if less than 10
             season_str = f'S{specific_season:02d}'
             episode_str = f'E{count:02d}'
+
+            # Print the episode number and url
             print(f'Downloading {season_str}{episode_str} | url: {url}')
             
             tries = 0
             while tries < self.maximum_tries:
                 try:
-                    tries += 1
-    
                     # Download the video using youtube-dl
-                    output = subprocess.check_output(['youtube-dl', '--no-call-home', '-o', f'{tvshow_title}/%(title)s.%(ext)s', '--verbose', url], stderr=subprocess.STDOUT)
-                    filename = output.decode('iso-8859-1').strip()  # Get last line of output
+                    output = subprocess.check_output(
+                        [
+                            'youtube-dl', '--no-call-home', '-o',
+                            f'{tvshow_title}/%(title)s.%(ext)s', '--verbose', url
+                        ], stderr=subprocess.STDOUT)
 
-                    # Get the current filename of the media file from the output
-                    match = re.search(r'\[ffmpeg\] Merging formats into "(.*?)"', filename)
-                    if match: 
-                        filename = match.group(1)
-                    else: 
-                        print(f'Error downloading {season_str}{episode_str} | url: {url}')
-                                        
+                    # Decode the output and send it to the get_filename function
+                    console_output = output.decode('iso-8859-1').strip()
+                    filename = self.get_filename(console_output, tvshow_title, url)
+                    
                     # Get the new filename with the right format
                     file_ext = os.path.splitext(filename)[1]
-                    new_filename = f'{tvshow_title}.{season_str}{episode_str}{file_ext}'
-                    
-                    # Move the downloaded file to the right directory with the right filename
+
+                    # Create the season directory if it doesn't exist
                     os.makedirs(os.path.join(tvshow_title, f'Season {specific_season}'), exist_ok=True)
 
-                    # Use zero-padded strings for season and episode numbers if less than 10 to get for example S01E01 instead of S1E1
-                    if specific_season < 10:
-                        new_filename = f'{tvshow_title}.{season_str}{episode_str}'
-                    else:
-                        new_filename = f'{tvshow_title}.{season_str}{episode_str}'
+                    # Convert the file to mkv if the user wants to and the file is an mp4
+                    if self.convert_media.lower() == 'y' and file_ext == '.mp4':
+                        output_filename = os.path.splitext(filename)[0] + '.mkv'
+                        subprocess.check_output(['ffmpeg', '-i', filename, '-c', 'copy', output_filename], stderr=subprocess.STDOUT)
+                        os.remove(filename)
+                        filename = output_filename
 
-                    # Rename the file
-                    os.rename(filename, f'{tvshow_title}\\Season {specific_season}\\' + new_filename + file_ext)
+                    # Rename the file and move it to the right directory
+                    new_file_ext = os.path.splitext(filename)[1]
+
+                    # New filename with the right format
+                    new_filename = f'{tvshow_title}.{season_str}{episode_str}{new_file_ext}'
+
+                    # Create the new path and rename the file to the new path
+                    new_path = f'{tvshow_title}/Season {specific_season}/{new_filename}'
+                    os.rename(filename, new_path)
                     
-                    # Break out of the retry loop if the download was successful
+                    # Break out of the retry loop if the download was successful                                                        
                     break
 
                 except:
@@ -218,7 +254,38 @@ class DRTVScraper:
                     if tries == self.maximum_tries:
                         print(f'Error downloading {season_str}{episode_str} | url: {url}')
                     else:
-                        print(f'Retrying {season_str}{episode_str} ({tries}/{self.maximum_tries}) | url: {url}')
+                        tries += 1
+                        print(f'Retrying {season_str}{episode_str} - {tries}/{self.maximum_tries} tries | url: {url}')
+
+
+    def scrape_movie(self, movie_title, movie_link):
+        # Create the movie directory if it doesn't exist
+        os.makedirs(movie_title, exist_ok=True)
+
+        # Download the movie using youtube-dl
+        print(f'\nDownloading {movie_title} | url: {movie_link}')
+        output = subprocess.check_output(['youtube-dl', '--no-call-home', '-o', f'{movie_title}/%(title)s.%(ext)s', '--verbose', movie_link], stderr=subprocess.STDOUT)
+       
+        # Get the filename of the downloaded file
+        filename = self.get_filename(output, movie_title, movie_link)
+
+        # Convert the file to mkv if the user wants to and the file is an mp4
+        file_ext = os.path.splitext(filename)[1]
+        if self.convert_media.lower() == 'y':
+            if file_ext == '.mp4':
+                output_filename = os.path.splitext(filename)[0] + '.mkv'
+                subprocess.check_output(['ffmpeg', '-i', filename, '-c', 'copy', output_filename], stderr=subprocess.STDOUT)
+                filename = filename.replace('.mp4', '.mkv')
+                os.remove(filename.replace('.mkv', '.mp4'))
+
+
+    def get_filename(self, output, title, link):
+        # Get the current filename of the media file from the output
+        match = re.search(r'\[ffmpeg\] Merging formats into "(.*?)"', output)
+        if match: 
+            return match.group(1)
+        else: 
+            print(f'Error downloading {title} | url: {link}')
 
 
     def get_corresponding_season(self, url):
